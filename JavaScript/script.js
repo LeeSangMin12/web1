2965a23e4f10ab1a172f3593cf21802bd8d2aec0
LeeSangMin12 <isangmin516@gmail.com>
2021-04-16 10:07:55 +0000

    hi

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,3 @@
-//var lis = document.getElementById("primary-slider").getElementsByTagName("li");
 function fetchPage(name){
     fetch(name).then(function(response){
       response.text().then(function(text){
@@ -6,40 +5,6 @@ function fetchPage(name){
       })
     });
   }
-
-
-
-// Create and mount the thumbnails slider.
-var secondarySlider = new Splide( '#secondary-slider', {
-	rewind      : true,
-	fixedWidth  : 100,
-	fixedHeight : 80,
-	isNavigation: true,
-	gap         : 10,
-	focus       : 'center',
-	pagination  : false,
-	cover       : true,
-	breakpoints : {
-		'600': {
-			fixedWidth  : 66,
-			fixedHeight : 40,
-		}
-	}
-} ).mount();
-
-// Create the main slider.
-var primarySlider = new Splide( '#primary-slider', {
-	type       : 'fade',
-	heightRatio: 0.5,
-	pagination : false,
-	arrows     : false,
-	cover      : true,
-} );
-// Set the thumbnails slider as a sync target and then call mount.
-primarySlider.sync( secondarySlider ).mount();
-
-			
-			
 
 
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
